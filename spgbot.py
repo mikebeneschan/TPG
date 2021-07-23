3ad67c46679188904d14fae00a735c2241792ec2
mikebeneschan <52139261+mikebeneschan@users.noreply.github.com>
2021-07-22 21:41:21 -0700

    Update spgbot.py

--- spgbot.py
+++ spgbot.py
@@ -1,25 +1,20 @@
-#so right now the bot responds with a randomly selected entry from a sql table
-#the sql table is from a postgresql database hosted locally on your comp 
 import discord
 #import nest_asyncio
 import random
 import psycopg2
-#import os
+import os
 
-#token = os.getenv('DISCORD_TOKEN')
+token = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 guild = discord.Guild
 
-#this part connects to the postgresql database
 conn = psycopg2.connect(
     host="localhost",
-    database="whatever your db name is",
+    database="CARDS",
     user="postgres",
-    password="idk your password lmao")
-#the cursor is the thing that executes sql queries on the postgresql database
+    password="password_here")
 cur = conn.cursor()
 
-#bootup for the discord bot (just to make sure it started)
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -28,22 +23,30 @@ async def on_ready():
     print('------')
 #nest_asyncio.apply()     
 
-#discord bot listens for the command
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith("spg.gacha"):
-        #lmao so i ended up using a string because i couldn't figure out how to pass an integer into the postgresql query but i mean it still works???
-        cardid = str(random.randint(1,10)) 
-        cur.execute("SELECT cardname,cardid FROM cards WHERE cardid=%s",(cardid,))
-        #technically cur.fetchone() only gets the first result from a sql query. but we're assuming that these queries only have 1 result, so this is fine
+    if (message.content=="spg.gacha"):
+        
+        cardid = str(random.randint(1,6)) 
+        cur.execute("SELECT image,cardname,cardid,tier,pet FROM cards WHERE cardid=%s",(cardid,))
         row = cur.fetchone()
-        await message.channel.send(row)
-
-client.run("token")
+        filename = row[0]
+        title = row[1]
+        cardid = str(row[2])
+        tier = row[3]
+        pet = row[4]
+        msg = "**Title: "+title+"**\n**ID: "+cardid+"**\n**Tier: "+tier+"**\n**Pet: "+pet+"**"
+        await message.channel.send(content=msg, file=discord.File(filename))
+        
+    if (message.content=="spg.new"):
+        cmd="INSERT INTO users(discord) VALUES ('"+str(message.author)+"')"
+        cur.execute(cmd)
+        conn.commit()
+        await message.channel.send("successfully added to user database")
+client.run(token)
 #nest_asyncio.apply()      
 
-#close the cursor and the postgresql connection
 cur.close()
 conn.close()
